f5f3006c5972b14dd1e40994646d1fa9e2f367b6
kurthablado <kurt.hablado@edu.sait.ca>
2024-07-31 09:38:18 -0600

    Update HomeScreen.jsx

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -8,6 +8,9 @@ export default function HomeScreen( { navigation } ) {
     return(
         <View>
             <Text>Wanna Save Me - Expense Tracker</Text>
+            <Button title="Home Screen" onPress={ () => navigation.navigate('HomeScreen') } />
+            <Button title="Account Balance" onPress={ () => navigation.navigate('AccountBalance') } />
+            <Button title="Transactions" onPress={ () => navigation.navigate('Transactions')} />    
         </View>
         
     );
