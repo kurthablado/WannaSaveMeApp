8810b7d189c728e7045cac333bb566755523782d
kurthablado <kurt.hablado@edu.sait.ca>
2024-07-31 08:51:44 -0600

    initial commit

---
+++ src/screens/AccountBalance.jsx
@@ -0,0 +1,11 @@
+// Account Balance Screen
+
+import { Button, Image, SafeAreaView, Text, View } from "react-native";
+
+export default function AccountBalanceScreen() {
+
+
+    return (
+        <View>Account Balance</View>
+    );
+}
---
+++ src/screens/Transactions.jsx
@@ -0,0 +1,12 @@
+// Transactions Screen
+
+import { Button, Image, SafeAreaView, Text, View } from "react-native";
+
+
+export default function TransactionsScreen() {
+    return (
+        <View>
+            <Text>Transactions</Text>
+        </View>
+    );
+}
