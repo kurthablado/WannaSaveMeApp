52cad0295b1fe9ae564170f0c14b27d67765dca5
Zena Kebede <zena.kebede@edu.sait.ca>
2024-08-02 20:26:13 -0600

    Added code for main functional component of AccountBalance

--- src/screens/AccountBalance.jsx
+++ src/screens/AccountBalance.jsx
@@ -1,12 +1,19 @@
 // Account Balance Screen
 
+import React, { useState } from "react";
 import { Button, Image, SafeAreaView, Text, View } from "react-native";
 
 export default function AccountBalanceScreen() {
 
     const [balance, setBalance] = useState(0);
+    const [expenseName, setExpenseName] = useState("");
+    const [expenseAmount, setExpenseAmount] = useState("");
+    const [expenseDate, setExpenseDate] = useState("");
 
-    
+    const handleAddExpense = () => {
+
+        console.log("Add Expense:", { expenseName, expenseAmount, expenseDate });
+    };
 
     return (
         <View>
